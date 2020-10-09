e1276194fa9691bc2cc8cd50129a8204658cdec6
bbeeco <70409516+bbeeco@users.noreply.github.com>
2020-10-09 17:34:15 -0400

    im so grrr

--- script.js
+++ script.js
@@ -1,61 +1,56 @@
-var allMovies = [];
-var console = {};
-
+let allMovies = [];
 
 //Movie object contructor
 function Movie(title, rating, havewatched) {
-    "use strict";
     this.title = title;
     this.rating = rating;
     this.havewatched = havewatched;
 }
 
 //add a movie OBJECT to the allMovies array
-function addMovie(movie) {
-    "use strict";
+function addMovie (movie) {
     allMovies.push(movie);
 }
 
 //iterate through all elements of allMovies array
 //print out to console in a correct format
 //print out the total number of movies in allMovies array
-function printMovies() {
-    "use strict";
-    var i;
-    for (i = 0; i < allMovies.length; i) {
-        console.log(allMovies[i]);
-        i = i + 1;
+function printMovies () {
+    var i, count;
+    count = 0;
+    for (i = 0; i < allMovies.length; i++) {
+        console.log(allMovies[i].name + ", rating of " + allMovies[i].rating + ", havewatched: " + allMovies[i].havewatched);
+        count = count + 1;
     }
+    console.log("You have " + i + " movies in total");
 }
 
 //print out to console, only the movies that has a rating higher than rating(argument)
 //print out the total number of matches
-function highRatings(rating) {
-    "use strict";
+function highRatings (rating) {
+    console.log("printing movie that has a rating higher than " + rating);
     var i, count;
     count = 0;
-    for (i = 0; i < allMovies.length; i) {
+    for (i = 0; i < allMovies.length; i++) {
         if (allMovies[i].rating >= rating) {
-            console.log(allMovies[i]);
-            i = i + 1;
+            console.log(allMovies[i].name + ", rating of " + allMovies[i].rating + ", havewatched: " + allMovies[i].havewatched);
             count = count + 1;
         }
     }
+    console.log("/nIn total, there are " + count + " matches");
 }
 
 //Toggle the 'haveWatched' property of the specified movie 
-function changeWatched(title) {
-    "use strict";
+function changeWatched (title) {
     var i;
     for (i = 0; i < allMovies.length; i) {
         if (allMovies[i].title === title) {
             if (allMovies[i].havewatched == true) {
                 allMovies[i].havewatched = false;
             }
-                else {
+            else {
                     allMovies[i].havewatched = true;
-                }
-                
+            }    
         }
     }
 }
@@ -64,11 +59,11 @@ function changeWatched(title) {
 
 ////////////////////////////////////////////////////////////
 //Test code - DO NOT DELETE OR EDIT
-var x = new Movie("Spiderman", 3, "true");
-var y = new Movie("Citizen Kane", 4, "false");
-var z = new Movie("Zootopia", 4.5, "true");
+let x = new Movie("Spiderman", 3, "true");
+let y = new Movie("Citizen Kane", 4, "false");
+let z = new Movie("Zootopia", 4.5, "true");
 
-allMovies.push(x, y, z);
+allMovies.push(x,y,z);
 
 console.log("----------------");
 console.log("running program......");
@@ -76,7 +71,7 @@ console.log("----------------");
 printMovies();
 
 
-var movie1 = new Movie("Parasite", 2, "false");
+let movie1 = new Movie("Parasite", 2, "false");
 
 
 console.log("----------------");
